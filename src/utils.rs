2ea6079e920f718b1272cf40add04f85a37d8820
Alex Pakalniskis 3 <alexpakalniskis@gmail.com>
2023-03-27 08:15:39 -0700

    chore: add generate entities page logic

--- src/utils.rs
+++ src/utils.rs
@@ -1,4 +1,6 @@
 use std::string::String;
+use std::fs::File;
+use std::io::Write;
 
 use graphql_parser::query::Type;
 
@@ -24,4 +26,61 @@ pub struct DefinitionField<'a> {
 pub struct DefinitionValue {
     pub name: String,
     pub description: Option<String>,
+}
+
+
+pub fn generate_entities_page(schema_definitions: &Vec<DefinitionResult<'static>>) -> Result<(), Box<dyn std::error::Error>> {
+
+    let path = "entities.md";
+
+    let mut markdown = String::from("# Subgraph Entities\n");
+
+    for def in schema_definitions {
+        markdown.push_str(&format!("* [{}](#{})\n", def.name, def.name.to_lowercase()));
+    }
+
+    markdown.push_str("\n");
+
+    for def in schema_definitions {
+        markdown.push_str(&format!("## {}\n", def.name));
+        match &def.description {
+            Some(d) => markdown.push_str(&format!("{}\n", d)),
+            None => (),
+        }
+        markdown.push_str("\n");
+
+        markdown.push_str("| Field/Value | Type | Description | \n");
+        markdown.push_str("| --- | --- | --- | \n");
+    
+
+    // if fields not the null placeholder
+        // add fields to entities table
+        for field in &def.fields {
+            if field.name != "None" {
+
+                match &field.description {
+                    Some(d) => markdown.push_str(&format!("| {} | {} | {} | \n", field.name, field.field_type, d)),
+                    None => markdown.push_str(&format!("| {} | {} | | \n", field.name, field.field_type)),
+                }
+            }
+        }
+
+        // if values not the null placeholder
+        // add values to entities table
+        for value in &def.values {
+            if value.name != "None" {
+                match &value.description {
+                    Some(d) => markdown.push_str(&format!("| {} | | {} | \n", value.name, d)),
+                    None => markdown.push_str(&format!("| {} | | | \n", value.name)),
+                }
+            }
+        }
+
+        markdown.push_str("\n\n\n");
+
+    }
+
+    let mut output = File::create(path)?;
+    write!(output, "{}", &markdown)?;
+    Ok(())
 }
