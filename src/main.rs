38c77903e7811b4b52c1714139fbdfeffd586f53
Alex Pakalniskis 3 <alexpakalniskis@gmail.com>
2023-03-27 07:28:04 -0700

    chore: refactor to only schema parsing logic

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,101 @@
+mod utils;
+use crate::utils::{DefinitionResult, DefinitionField, DefinitionValue};
+use graphql_parser::schema::parse_schema;
+use graphql_parser::schema::TypeDefinition::{Scalar, Object, Interface, Union, Enum, InputObject};
+use graphql_parser::schema::Definition::{SchemaDefinition, TypeDefinition, TypeExtension, DirectiveDefinition};
+use graphql_parser::query::Type;
+
 use std::error::Error;
 
-mod utils;
-use crate::utils::{SubgraphManifest, DefinitionResult};
+fn main() -> Result<(), Box<dyn Error>>  {
+    // Read testing-schema.graphql
+    let contents = std::fs::read_to_string("./testing-schema.graphql")
+    .expect("Should have been able to read the file");
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn Error>> {
+    // Parse schema
+    let ast = parse_schema::<String>(&contents)?.to_owned();
 
-    let manifest_response = reqwest::get("https://ipfs.io/ipfs/QmbW34MGRyp7LWkpDyKXDLWsKrN8iqZrNAMjGTYHN2zHa1")
-    .await?
-    .text()
-    .await?;
+    // Traverse type definition options with match
+    let mut results: Vec<DefinitionResult> = Vec::new();
 
-    let manifest_data: SubgraphManifest = serde_yaml::from_str(&manifest_response).unwrap();
+    for definition in ast.definitions {
+        match definition {
+            TypeDefinition(Object(o)) => {
+                let mut tmp_fields: Vec<DefinitionField> = Vec::new();
+                for field in o.fields {
+                    tmp_fields.push(
+                        DefinitionField {
+                            name: field.name,
+                            description: field.description,
+                            field_type: field.field_type,
+                        }
+                    );
+                }
+                let mut tmp_values: Vec<DefinitionValue> = Vec::new();
+                tmp_values.push(
+                    DefinitionValue {
+                        name: "None".to_string(),
+                        description: None,
+                    }
+                );
+                results.push(
+                    DefinitionResult {
+                        name: o.name,
+                        description: o.description,
+                        fields: tmp_fields,
+                        values: tmp_values,
+                    }
+                );
+            },
+            TypeDefinition(Enum(e)) => {
+                let mut tmp_values: Vec<DefinitionValue> = Vec::new();
+                for value in e.values {
+                    tmp_values.push(
+                        DefinitionValue {
+                            name: value.name,
+                            description: value.description,
+                        }
+                    );
+                }
+                let mut tmp_fields: Vec<DefinitionField> = Vec::new();
+                tmp_fields.push(
+                    DefinitionField {
+                        name: "None".to_string(),
+                        description: None,
+                        field_type: Type::NamedType("None".to_string()),
+                    }
+                );
+                results.push(
+                    DefinitionResult {
+                        name: e.name,
+                        description: e.description,
+                        fields: tmp_fields,
+                        values: tmp_values,
+                    }
+                );
+            },
+            TypeDefinition(Scalar(s)) => {
+                println!("Coming soon");
+            },
+            TypeDefinition(Interface(i)) => {
+                println!("Coming soon");
+            },
+            TypeDefinition(Union(u)) => {
+                println!("Coming soon");
+            },
+            TypeDefinition(InputObject(io)) => {
+                println!("Coming soon");
+            },
+            SchemaDefinition(_) | TypeExtension(_) | DirectiveDefinition(_) => todo!(),
+        }
+    }
 
-    println!("{:?}", manifest_data);
+    // Print results
+    for res in results {
+        println!("{:?}", res);
+        println!("");
+    }
 
     Ok(())
 }
+
--- src/utils.rs
+++ src/utils.rs
@@ -1,55 +1,6 @@
-use std::collections::HashMap;
 use std::string::String;
-use serde::Deserialize;
 
 use graphql_parser::query::Type;
-
-
-#[allow(non_snake_case)]
-#[derive(Debug, Deserialize)]
-pub struct SubgraphManifest {
-    pub dataSources: Vec<DataSource>,
-    pub description: Option<String>,
-    pub repository: Option<String>,
-    pub specVersion: String,
-    pub schema: SchemaAddress,
-}
-
-#[allow(non_snake_case)]
-#[derive(Debug, Deserialize)]
-pub struct SchemaAddress {
-    pub file: HashMap<String, String>,
-}
-
-#[allow(non_snake_case)]
-#[derive(Debug, Deserialize)]
-pub struct DataSource {
-    pub kind: String,
-    pub mapping: Mapping,
-    pub name: String,
-    pub network: String,
-    pub source: Source,
-}
-
-#[allow(non_snake_case)]
-#[derive(Debug, Deserialize)]
-pub struct Mapping {
-    pub abis: serde_yaml::Sequence,
-    pub apiVersion: String,
-    pub entities: serde_yaml::Sequence,
-    pub eventHandlers: serde_yaml::Sequence,
-    pub file: HashMap<String, String>,
-    pub kind: String,
-    pub language: String,
-}
-
-#[allow(non_snake_case)]
-#[derive(Debug, Deserialize)]
-pub struct Source {
-    pub abi: String,
-    pub address: String,
-    pub startBlock: u32,
-}
 
 #[allow(non_snake_case)]
 #[derive(Debug)]
